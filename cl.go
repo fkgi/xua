13ef0fc091377443b639178cdf846649dfced6b7
fkgi <ats.fkgi@gmail.com>
2022-09-23 15:52:20 +0900

    modify

--- cl.go
+++ cl.go
@@ -2,109 +2,13 @@ package xua
 
 import (
 	"bytes"
-	"encoding/binary"
 	"io"
-	"strconv"
 )
 
 /*
 CL: SCCP Connectionless (CL) Messages
 Message class = 0x07
 */
-
-/*
-address of SCCP
-
-	 0                   1                   2                   3
-	 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	|      Routing Indicator        |       Address Indicator       |
-	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	/                       Address parameter(s)                    /
-	\                                                               \
-	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
-Global Title
-
-	0                 1                   2                   3
-	0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	|         Tag = 0x8001          |            Length             |
-	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	|                Reserved                       |      GTI      |
-	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	|   No. Digits  | Trans. type   |    Num. Plan  | Nature of Add |
-	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	/                         Global Title Digits                   /
-	\                                                               \
-	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-*/
-type address struct {
-	ri uint16
-	ai uint16
-
-	gti uint8
-	tt  uint8
-	npi uint8
-	nai uint8
-	gt  string
-
-	pc  uint32
-	ssn uint8
-}
-
-func (a *address) marshal(w io.Writer, id uint16) {
-	buf := new(bytes.Buffer)
-
-	if len(a.gt) != 0 {
-		l := len(a.gt)
-		if l%8 != 0 {
-			l += 8 - l%8
-		}
-		l += 12
-		binary.Write(buf, binary.BigEndian, uint16(0x8001))
-		binary.Write(buf, binary.BigEndian, uint16(l))
-		binary.Write(buf, binary.BigEndian, uint32(a.gti))
-		buf.WriteByte(uint8(len(a.gt)))
-		buf.WriteByte(a.tt)
-		buf.WriteByte(a.npi)
-		buf.WriteByte(a.nai)
-
-		l = len(a.gt)
-		for i := 0; i < l; i++ {
-			var b byte
-			d, e := strconv.Atoi(string(a.gt[i]))
-			if e != nil {
-				d = 0
-			}
-			b = byte(d)
-			i++
-			if i < l {
-				d, e = strconv.Atoi(string(a.gt[i]))
-				if e != nil {
-					d = 0
-				}
-				b |= byte(d << 4)
-			}
-			buf.WriteByte(b)
-		}
-		if l%8 != 0 {
-			buf.Write(make([]byte, 8-l%8))
-		}
-	}
-	if a.pc != 0 {
-		writeUint32(buf, 0x8002, a.pc)
-	}
-	if a.ssn != 0 {
-		writeUint8(buf, 0x8003, a.ssn)
-	}
-
-	binary.Write(w, binary.BigEndian, id)
-	binary.Write(w, binary.BigEndian, uint16(8+buf.Len()))
-	binary.Write(w, binary.BigEndian, a.ri)
-	binary.Write(w, binary.BigEndian, a.ai)
-	buf.WriteTo(w)
-}
 
 /*
 CLDT is Connectionless Data Transfer message. (Message type = 0x01)
@@ -171,9 +75,9 @@ type CLDT struct {
 	cdpa          address
 	sequenceCtrl  uint32
 
-	hopCount   uint8
-	importance *uint8
-	priority   *uint8
+	// hopCount   uint8
+	// importance *uint8
+	// priority   *uint8
 	// correlation *uint32
 
 	// first      bool
@@ -183,11 +87,9 @@ type CLDT struct {
 	data []byte
 }
 
-func (m *CLDT) handleMessage() {
-}
+func (m *CLDT) handleMessage() {}
 
-func (m *CLDT) handleResult(msg message) {
-}
+func (m *CLDT) handleResult(msg message) {}
 
 func (m *CLDT) marshal() (uint8, uint8, []byte) {
 	buf := new(bytes.Buffer)
@@ -209,24 +111,22 @@ func (m *CLDT) marshal() (uint8, uint8, []byte) {
 	m.cdpa.marshal(buf, 0x0103)
 
 	// Sequence Control
-	if m.sequenceCtrl != 0 {
-		writeUint32(buf, 0x0116, m.sequenceCtrl)
-	}
+	writeUint32(buf, 0x0116, m.sequenceCtrl)
 
 	// SS7 Hop Count (Optional)
-	if m.hopCount != 0 {
-		writeUint8(buf, 0x0101, m.hopCount)
-	}
+	// if m.hopCount != 0 {
+	// 	writeUint8(buf, 0x0101, m.hopCount)
+	// }
 
 	// Importance (Optional)
-	if m.importance != nil {
-		writeUint8(buf, 0x0113, *m.importance)
-	}
+	// if m.importance != nil {
+	// 	writeUint8(buf, 0x0113, *m.importance)
+	// }
 
 	// Message Priority (Optional)
-	if m.priority != nil {
-		writeUint8(buf, 0x0114, *m.priority)
-	}
+	// if m.priority != nil {
+	//	writeUint8(buf, 0x0114, *m.priority)
+	// }
 
 	// Correlation ID (Optional)
 	// if m.correlation != nil {
@@ -258,6 +158,34 @@ func (m *CLDT) unmarshal(t, l uint16, r io.ReadSeeker) (e error) {
 	case 0x0115:
 		// Protocol Class
 		m.protocolClass, e = readUint8(r, l)
+		m.returnOnError = m.protocolClass&0x80 == 0x80
+		m.protocolClass = m.protocolClass & 0x7F
+	case 0x0102:
+		// Source Address
+		m.cgpa, e = readAddress(r, l)
+	case 0x0103:
+		// Destination Address
+		m.cdpa, e = readAddress(r, l)
+	case 0x0116:
+		// Sequence Control
+		m.sequenceCtrl, e = readUint32(r, l)
+	// case 0x0101:
+	//	// SS7 Hop Count (Optional)
+	//	m.hopCount, e = readUint8(r, l)
+	// case 0x0113:
+	//	// Importance (Optional)
+	//	var tmp uint8
+	//	if tmp, e = readUint8(r, l); e == nil {
+	//		m.importance = &tmp
+	//	}
+	// case 0x0114:
+	//	// Message Priority (Optional)
+	//	var tmp uint8
+	//	if tmp, e = readUint8(r, l); e == nil {
+	//		m.priority = &tmp
+	//	}
+	case 0x010B:
+		m.data, e = readData(r, l)
 	default:
 		_, e = r.Seek(int64(l), io.SeekCurrent)
 	}
@@ -291,15 +219,15 @@ TxCLDR is Connectionless Data Response message. (Message type = 0x02)
 	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 	|         Tag = 0x0101          |             Length            |
 	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	|                         SS7 Hop Count                         |
+	|              Reserved                         | SS7 Hop Count |
 	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 	|         Tag = 0x0113          |             Length            |
 	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	|                          Importance                           |
+	|                Reserved                       |   Importance  |
 	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 	|         Tag = 0x0114          |             Length            |
 	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	|                      Message Priority                         |
+	|              Reserved                         |  Msg Priority |
 	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 	|         Tag = 0x0013          |             Length            |
 	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -307,7 +235,7 @@ TxCLDR is Connectionless Data Response message. (Message type = 0x02)
 	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 	|         Tag = 0x0117          |            Length             |
 	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	|                          Segmentation                         |
+	| first/remain  |             Segmentation Reference            |
 	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 	|         Tag = 0x010b          |             Length            |
 	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
@@ -323,9 +251,9 @@ type CLDR struct {
 	cgpa  address
 	cdpa  address
 
-	hopCount   uint8
-	importance *uint8
-	priority   *uint8
+	// hopCount   uint8
+	// importance *uint8
+	// priority   *uint8
 	// correlation *uint32
 
 	// first      bool
@@ -335,17 +263,91 @@ type CLDR struct {
 	data []byte
 }
 
-func (m *CLDR) handleMessage() {
-}
-func (m *CLDR) handleResult(msg message) {
-}
+func (m *CLDR) handleMessage()           {}
+func (m *CLDR) handleResult(msg message) {}
 func (m *CLDR) marshal() (uint8, uint8, []byte) {
 	buf := new(bytes.Buffer)
+
+	// Routing Context
+	writeRoutingContext(buf, m.ctx)
+
+	// SCCP Cause
+	writeUint32(buf, 0x0106, m.cause)
+
+	// Source Address
+	m.cgpa.marshal(buf, 0x0102)
+
+	// Destination Address
+	m.cdpa.marshal(buf, 0x0103)
+
+	// SS7 Hop Count (Optional)
+	// if m.hopCount != 0 {
+	// 	writeUint8(buf, 0x0101, m.hopCount)
+	// }
+
+	// Importance (Optional)
+	// if m.importance != nil {
+	// 	writeUint8(buf, 0x0113, *m.importance)
+	// }
+
+	// Message Priority (Optional)
+	// if m.priority != nil {
+	//	writeUint8(buf, 0x0114, *m.priority)
+	// }
+
+	// Correlation ID (Optional)
+	// if m.correlation != nil {
+	//	writeUint32(buf, 0x0013, *m.correlation)
+	// }
+
+	// Segmentation (Optional)
+	// if m.segmentRef != nil {
+	//	var v uint32
+	//	if m.first {
+	//		v |= 0x80000000
+	//	}
+	//	v |= uint32(m.remain) << 32
+	//	v |= *m.segmentRef
+	//	writeUint32(buf, 0x0117, v)
+	// }
+
+	// Data
+	writeData(buf, m.data)
+
 	return 0x07, 0x02, buf.Bytes()
 }
 
 func (m *CLDR) unmarshal(t, l uint16, r io.ReadSeeker) (e error) {
 	switch t {
+	case 0x0006:
+		// Routing Context
+		m.ctx, e = readRoutingContext(r, l)
+	case 0x0106:
+		// Sequence Control
+		m.cause, e = readUint32(r, l)
+	case 0x0102:
+		// Source Address
+		m.cgpa, e = readAddress(r, l)
+	case 0x0103:
+		// Destination Address
+		m.cdpa, e = readAddress(r, l)
+		// case 0x0101:
+		//	// SS7 Hop Count (Optional)
+		//	m.hopCount, e = readUint8(r, l)
+		// case 0x0113:
+		//	// Importance (Optional)
+		//	var tmp uint8
+		//	if tmp, e = readUint8(r, l); e == nil {
+		//		m.importance = &tmp
+		//	}
+		// case 0x0114:
+		//	// Message Priority (Optional)
+		//	var tmp uint8
+		//	if tmp, e = readUint8(r, l); e == nil {
+		//		m.priority = &tmp
+		//	}
+	case 0x010B:
+		m.data, e = readData(r, l)
 	default:
 		_, e = r.Seek(int64(l), io.SeekCurrent)
 	}
--- tlv.go
+++ tlv.go
@@ -1,9 +1,11 @@
 package xua
 
 import (
+	"bytes"
 	"encoding/binary"
 	"errors"
 	"io"
+	"strconv"
 )
 
 /*
@@ -124,5 +126,191 @@ func writeData(w io.Writer, d []byte) {
 func readData(r io.ReadSeeker, l uint16) (d []byte, e error) {
 	d = make([]byte, l)
 	_, e = r.Read(d)
+	return
+}
+
+/*
+address of SCCP
+
+	 0                   1                   2                   3
+	 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	|      Routing Indicator        |       Address Indicator       |
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	/                       Address parameter(s)                    /
+	\                                                               \
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+Global Title
+
+	0                 1                   2                   3
+	0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	|         Tag = 0x8001          |            Length             |
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	|                Reserved                       |      GTI      |
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	|   No. Digits  | Trans. type   |    Num. Plan  | Nature of Add |
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	|2 addr.|1 addr.|4 addr.|3 addr.|6 addr.|5 addr.|8 addr.|7 addr.|
+	|  sig. | sig.  |  sig. | sig.  |  sig. | sig.  |  sig. | sig.  |
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	|        .............          |filler |N addr.|   filler      |
+	|                               |if req | sig.  |               |
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+Point Code
+
+	0                   1                   2                   3
+	0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	|         Tag = 0x8002          |            Length = 8         |
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	|                            Point Code                         |
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+Subsystem Number
+
+	0                   1                   2                   3
+	0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	|         Tag = 0x8003          |            Length = 8         |
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	|                 Reserved                      |   SSN value   |
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+*/
+type address struct {
+	ri uint16
+	ai uint16
+
+	gti uint8
+	tt  uint8
+	npi uint8
+	nai uint8
+	gt  string
+
+	pc  uint32
+	ssn uint8
+}
+
+func (a *address) marshal(w io.Writer, id uint16) {
+	buf := new(bytes.Buffer)
+
+	if len(a.gt) != 0 {
+		l := len(a.gt)
+		if l%8 != 0 {
+			l += 8 - l%8
+		}
+		l += 12
+		binary.Write(buf, binary.BigEndian, uint16(0x8001))
+		binary.Write(buf, binary.BigEndian, uint16(l))
+		binary.Write(buf, binary.BigEndian, uint32(a.gti))
+		buf.WriteByte(uint8(len(a.gt)))
+		buf.WriteByte(a.tt)
+		buf.WriteByte(a.npi)
+		buf.WriteByte(a.nai)
+
+		l = len(a.gt)
+		for i := 0; i < l; i++ {
+			var b byte
+			d, e := strconv.Atoi(string(a.gt[i]))
+			if e != nil {
+				d = 0
+			}
+			b = byte(d)
+			i++
+			if i < l {
+				d, e = strconv.Atoi(string(a.gt[i]))
+				if e != nil {
+					d = 0
+				}
+				b |= byte(d << 4)
+			}
+			buf.WriteByte(b)
+		}
+		if l%8 != 0 {
+			buf.Write(make([]byte, 8-l%8))
+		}
+	}
+	if a.pc != 0 {
+		writeUint32(buf, 0x8002, a.pc)
+	}
+	if a.ssn != 0 {
+		writeUint8(buf, 0x8003, a.ssn)
+	}
+
+	binary.Write(w, binary.BigEndian, id)
+	binary.Write(w, binary.BigEndian, uint16(8+buf.Len()))
+	binary.Write(w, binary.BigEndian, a.ri)
+	binary.Write(w, binary.BigEndian, a.ai)
+	buf.WriteTo(w)
+}
+
+func readAddress(r io.ReadSeeker, l uint16) (a address, e error) {
+	if l%4 != 0 {
+		e = errors.New("invalid lenght of parameter")
+		return
+	}
+	if e = binary.Read(r, binary.BigEndian, &a.ri); e != nil {
+		return
+	}
+	if e = binary.Read(r, binary.BigEndian, &a.ai); e != nil {
+		return
+	}
+
+	buf := make([]byte, l-4)
+	if _, e = r.Read(buf); e != nil {
+		return
+	}
+	rr := bytes.NewReader(buf)
+	for rr.Len() > 8 {
+		var t, l uint16
+		if e = binary.Read(rr, binary.BigEndian, &t); e != nil {
+			break
+		}
+		if e = binary.Read(rr, binary.BigEndian, &l); e != nil {
+			break
+		}
+		l -= 4
+
+		switch t {
+		case 0x8001:
+			// GT
+			if l < 8 {
+				e = errors.New("invalid lenght of parameter")
+				break
+			}
+			gthdr := make([]byte, 8)
+			if _, e = rr.Read(gthdr); e != nil {
+				break
+			}
+			a.gti = gthdr[3]
+			a.tt = gthdr[5]
+			a.npi = gthdr[6]
+			a.nai = gthdr[7]
+
+			buf = make([]byte, l-8)
+			for i := 0; i < int(gthdr[4]); i++ {
+				g := buf[(i-(i%2))/2]
+				if i%2 == 0 {
+					a.gt = a.gt + strconv.Itoa(int(g&0x0F))
+				} else {
+					a.gt = a.gt + strconv.Itoa(int(g&0xF0>>4))
+				}
+			}
+		case 0x8002:
+			// PC
+			a.pc, e = readUint32(rr, l)
+		case 0x8003:
+			// SSN
+			a.ssn, e = readUint8(rr, l)
+		default:
+			_, e = r.Seek(int64(l), io.SeekCurrent)
+		}
+		if e != nil {
+			break
+		}
+	}
+
 	return
 }
